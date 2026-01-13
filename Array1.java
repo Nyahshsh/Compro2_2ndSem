2251cb17a506ba3bf2b47814144289492f5305b9
Nyahshsh <janiyah.tan@lorma.edu>
2026-01-13 12:22:20 +0800

    ArrayActivity1

---
+++ Array1.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class Array1 {
+    public static void main(String[] args){
+
+        // Create Array
+        int[] numbers = {3, 6, 9, 67, 69, 39, 79, 96, 99, 77};
+
+        boolean found = false;
+        int foundIndex = -1;
+
+        // Scanner for User Input
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number baby ;> ");
+        int input = sc.nextInt();
+        
+        // Create For Loop
+        for (int i = 0; i < numbers.length; i++){
+
+            //Find the user's number in the Array
+            if (numbers[i] == input){
+                found = true;
+                foundIndex = i;
+                break;
+            }
+        }
+
+        // Display of Index
+        if (found){
+            System.out.println(input + " was found at the index " + foundIndex + " baby :>");
+        }
+        else {
+            System.out.println(input + " was not found in the array baby :<");
+        }
+
+    }
+}
